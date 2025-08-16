4d10ba7b5b481ed1c405c48c68bd8b18032f29c0
Layson Ndenza <laysonndenza@gmail.com>
2025-08-16 22:59:02 +0300

    initial commit

---
+++ src/main/java/com/crntech/Bank_branches_management_backend/BankBranchesManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crntech.Bank_branches_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBranchesManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBranchesManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crntech/Bank_branches_management_backend/BankBranchesManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crntech.Bank_branches_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBranchesManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
